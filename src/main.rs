941b0e9f7f434225c1ca48c40183dae7452bb624
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2020-07-27 18:38:42 -0700

    Add point plotting

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,10 @@ extern crate minifb;
 use minifb::{Key, Window, WindowOptions};
 
 mod renderer;
-use renderer::*;
+use renderer::Renderer;
 
 fn main() {
-    let r = Renderer {
-        width: 1920,
-        height: 1080,
-    };
-    let mut buffer: Vec<u32> = vec![0; r.width * r.height];
-
+    let mut r = Renderer::new(1920, 1080);
     let mut window = Window::new(
         "Wiggly Renderer",
         r.width,
@@ -21,15 +16,15 @@ fn main() {
         panic!("Error opening window: {}", err);
     });
 
-    println!("Starting window event loop with renderer: {:?}", r);
+    println!("Starting window event loop");
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        r.render(&mut buffer).unwrap_or_else(|err| {
+        r.draw().unwrap_or_else(|err| {
             panic!("Error in renderer {:?}: {}", r, err);
         });
 
         window
-            .update_with_buffer(&buffer, r.width, r.height)
+            .update_with_buffer(&r.buffer, r.width, r.height)
             .unwrap_or_else(|err| {
                 panic!("Error updating window buffer: {}", err);
             });
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,15 +1,41 @@
+extern crate nalgebra as na;
 #[derive(Debug)]
 pub struct Renderer {
     pub width: usize,
     pub height: usize,
+    pub buffer: Vec<u32>,
 }
 
+type Color = na::Vector3<u8>;
+type Point2 = na::Vector2<f32>;
+
 impl Renderer {
-    pub fn render(&self, buffer: &mut Vec<u32>) -> Result<(), String> {
-        for i in buffer.iter_mut() {
-            // Draw blue
-            *i = 0x000000FF; // __RRGGBB
+    pub fn new(width: usize, height: usize) -> Renderer {
+        Renderer {
+            width,
+            height,
+            buffer: vec![0; width * height],
+        }
+    }
+    pub fn draw(&mut self) -> Result<(), String> {
+        // for i in self.buffer.iter_mut() {
+        //     // Draw blue
+        //     *i = 0x000000FF; // __RRGGBB
+        // }
+        for i in 0..self.width {
+            for j in 0..self.height {
+                self.plot(Point2::new(i as f32, j as f32), Color::new(255, 255, 0));
+            }
         }
         Ok(())
+    }
+
+    // Plots a single pixel with the given color
+    fn plot(&mut self, pos: Point2, col: Color) {
+        let index = pos.y as usize * self.width + pos.x as usize;
+        let pixel = &mut self.buffer[index];
+        *pixel = (col.x as u32) << 16 as u32;
+        *pixel |= (col.x as u32) << 8 as u32;
+        *pixel |= col.z as u32;
     }
 }
